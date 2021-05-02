c30ccfed3063f2121bf1ec8c3b86036ddf6b0140
nlilu <nlilum.kim@gmail.com>
2021-05-02 18:18:26 -0400

    cleaned up some code in index.js

--- index.js
+++ index.js
@@ -6,122 +6,120 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 //importing questions 
-const EmployeeQuestions = require('./lib/EmployeeQuestions');
 const EngineerQuestions = require('./lib/EngineerQuestions');
 const ManagerQuestions = require('./lib/ManagerQuestions');
 const InternQuestions = require('./lib/InternQuetions');
-const pageHtml = require('./lib/html-template');
 
 const { writeFile, copyFile } = require('./util/generate-site');
 
 //importing layout template file
 const generatePage = require('./src/page-template');
 //Array to hold team members
-let teamProfileArr = [];
-//Add intern profile 
-function addIntern () {
-    inquirer.prompt (InternQuestions)
-    .then(function(data) {
-        const internName = data.internName;
-        const internId = data. internId;
-        const internEmail = data.internEmail;
-        const internSchool = data.internSchool;
+const teamProfile = {
+    manager: [],
+    engineer: [],
+    intern: [],
+};
+
+
+//Add an additional Employee questions: 
+const promptEmployeeQuestion = () => {
+    return inquirer.prompt([
+    {
+        type: 'list',
+        name: "add Employees",
+        message: "Please choose an option below.",
+        choices: ["Add an Engineer", "Add an Intern", "None"],
+    },
+    ])
+    .then((addEmployeesData) => { 
+        if(addEmployeesData.addEmployee === "Add an Engineer") {
+            return promptEngineer(teamProfile);
+        } else if (addEmployeesData.addEmployee === "Add an Intern"){
+            return promptIntern(teamProfile);
+        } else {
+            return teamProfile;
+        }
+    });
+};
+//function that prompt user answers the engineer's questions 
+const promptEngineer = () => {
+    return inquirer.prompt(EngineerQuestions)
+    .then((engineerData) => {
+        const {
+            engineerName, 
+            engineerId,
+            engineerEmail,
+            engineerGithub,
+        } = engineerData;
+        const engineer = new Engineer (
+            engineerName, 
+            engineerId, 
+            engineerEmail,
+            engineerGithub,
+        );
+        teamProfile.engineer.push(engineer);
+    })
+    .then(promptEmployeeQuestion);
+};
+//prompts users with intern questions 
+const promptIntern = () => {
+    return inquirer.prompt(InternQuestions)
+    .then((internData) => {
+        const {
+            internName, 
+            internId, 
+            internEmail,
+            internSchool,
+        } = internData;
         const intern = new Intern (
             internName,
             internId,
             internEmail,
             internSchool,
         );
-        teamProfileArr.push(intern);
-
-        addOptions()
-    });
+        teamProfile.intern.push(intern);
+    })
+    .then(promptEmployeeQuestion);
 };
 
-//add engineer profiles to the team profile array
-function addEngineer () {
-    inquirer.prompt(EngineerQuestions)
-    .then(function(data) {
-        const engineerName = data.engineerName; 
-        const engineerId = data.engineerId;
-        const engineerEmail = data.engineerEmail;
-        const engineerGithub = data.engineerGithub;
-        const engineer = new Engineer (
-            engineerName,
-            engineerId, 
-            engineerEmail,
-            engineerGithub,
-        );
-        teamProfileArr.push(engineer);
-        
-        addOptions()
-    });
-};
-//add manager profiles to team profile array 
-function addManager () {
-    inquirer.prompt(ManagerQuestions)
-    .then(function(data) {
-        const managerName = data.managerName;
-        const managerId = data.managerId;
-        const managerEmail = data.managerEmail;
-        const managerOfficeNum = data.managerOfficeNum;
+
+//user prompts with manager questions 
+const promptManager = () => {
+    return inquirer.prompt(ManagerQuestions)
+    .then((managerData) => {
+        const {
+            managerName, 
+            managerId,
+            managerEmail,
+            managerOfficeNum,
+        } = managerData;
         const manager = new Manager (
             managerName,
             managerId,
             managerEmail,
             managerOfficeNum,
         );
-        teamProfileArr.push(manager);
-
-        addOptions()
-
+        teamProfile.manager.push(manager);
     });
 };
-//call the appropriate function based on the user's choice
-function addOptions () {
-    inquirer.prompt(EmployeeQuestions)
-    .then(function(data){
-        switch(data.Choices){
-            case "Add an Engineer":
-                addEngineer ();
-            break;
-            case "Add an Intern":
-                addIntern ();
-            break;
-            case "None":
-                generateHtmlFile ();
-            break;
-        };
-    });
-};
-
- function init () {
-     inquirer.prompt ([
-         {
-             type: "input",
-             name: "team Name",
-             message: "Welcome to Team Profile Generator!",
-             validate: teamNameInput => {
-                 if(teamNameInput && teamNameInput.trim().length > 0) {
-                     return true;
-                 } else {
-                     console.log("Must enter in Team Name!");
-                     return false;
-                 };
-             },
-            
-             },
-
-         
-     ])
-     .then(function(data) {
-         const teamName = data.teamName;
-         teamProfileArr.push(teamName);
-         addManager();
-     });
- };
-
- init ();
-
-
-       
+promptManager ()
+.then(promptEmployeeQuestion)
+.then(teamProfile => {
+    return generatePage(teamProfile);
+})
+.then( pageHTML => {
+    return writeFile(pageHTML);
+})
+    .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+})
+.then((copyFileReponse) => {
+    console.log(copyFileReponse);
+})
+.catch((err) => {
+    console.log(err);
+});
+ 
+ 
