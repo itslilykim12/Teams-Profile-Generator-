705c02dae0be838ffc40d2479906a72ace0762e2
nlilu <nlilum.kim@gmail.com>
2021-05-02 01:04:21 -0400

    failed employee test

---
+++ _tests_/employee.test.js
@@ -0,0 +1,27 @@
+const Employee = require('../lib/Employee');
+//getName, getId, getEmail
+test("creates an employee object", () => {
+    const employee = new Employee ("Madison", 005, "madison@gmail.com");
+
+    expect(employee.name).toBe("Madison");
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test("gets the Employee's name", () =>{
+    const employee = new Employee ("Madison", 005, "madison@gmail.com");
+
+    expect(employee.getName()).toBe("Madison");
+});
+
+test("gets Employee's ID", () =>{
+    const employee = new Employee ("Madison", 005, "madison@gmail.com");
+
+    expect(employee.getId()).toEqual(005);
+});
+
+test("gets Employee's email", () => {
+    const employee = new Employee ("Madison", 005, "madison@gmail.com");
+
+    expect(employee.getEmail()).toEqual('madison@gmail.com');
+});
